7d5094559a681cecbb13defee93f639f45e97033
davnon-1 <205902085+davnon-1@users.noreply.github.com>
2025-11-04 13:13:56 +0100

    Update TDS_sensor_code.ino
    Add temperature compensation and calibrated k-value

--- Sensor code/TDS_sensor_code.ino
+++ Sensor code/TDS_sensor_code.ino
@@ -1,10 +1,13 @@
+#include <OneWire.h>
+int DS18S20_Pin = 49; //DS18S20 Signal pin on digital 49
+OneWire ds(DS18S20_Pin);
 #define TdsSensorPin A1
 #define VREF 3.3      // analog reference voltage(Volt) of the ADC
 #define SCOUNT  30           // sum of sample point
 int analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
 int analogBufferTemp[SCOUNT];
 int analogBufferIndex = 0,copyIndex = 0;
-float averageVoltage = 0,tdsValue = 0,temperature = 25;
+float averageVoltage = 0,tdsValue = 0,temperature = 20, kValue = 0.88; //kValue comes from calibration of the sensor
 
 void setup()
 {
@@ -31,9 +34,10 @@ void loop()
       for(copyIndex=0;copyIndex<SCOUNT;copyIndex++)
         analogBufferTemp[copyIndex]= analogBuffer[copyIndex];
       averageVoltage = getMedianNum(analogBufferTemp,SCOUNT) * (float)VREF / 4096.0; // read the analog value more stable by the median filtering algorithm, and convert to voltage value
+      temperature = getTemp();
       float compensationCoefficient=1.0+0.02*(temperature-25.0);    //temperature compensation formula: fFinalResult(25^C) = fFinalResult(current)/(1.0+0.02*(fTP-25.0));
       float compensationVolatge=averageVoltage/compensationCoefficient;  //temperature compensation
-      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5; //convert voltage value to tds value
+      tdsValue=(133.42*compensationVolatge*compensationVolatge*compensationVolatge - 255.86*compensationVolatge*compensationVolatge + 857.39*compensationVolatge)*0.5*kValue; //convert voltage value to tds value
       //Serial.print("voltage:");
       //Serial.print(averageVoltage,2);
       //Serial.print("V   ");
@@ -65,4 +69,40 @@ int getMedianNum(int bArray[], int iFilterLen)
       else
 	bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
       return bTemp;
+}
+
+float getTemp(){
+  //returns the temperature from one DS18S20 in DEG Celsius
+  byte data[12];
+  byte addr[8];
+  if ( !ds.search(addr)) {
+  //no more sensors on chain, reset search
+  ds.reset_search();
+  return -1000;
+  }
+  if ( OneWire::crc8( addr, 7) != addr[7]) {
+  Serial.println("CRC is not valid!");
+  return -1000;
+  }
+
+  if ( addr[0] != 0x10 && addr[0] != 0x28) {
+  Serial.print("Device is not recognized");
+  return -1000;
+  }
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+  byte present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read Scratchpad
+
+  for (int i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+  ds.reset_search();
+  byte MSB = data[1];
+  byte LSB = data[0];
+  float tempRead = ((MSB << 8) | LSB); //using two's compliment
+  float TemperatureSum = tempRead / 16;
+  return TemperatureSum;
 }
